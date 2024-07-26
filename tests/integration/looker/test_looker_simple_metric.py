652645215fef47c128d88c278636419598fbe8bb
benw-at-birdie <55486471+benw-at-birdie@users.noreply.github.com>
2024-07-26 10:39:28 +0100

    refactor: format code

--- tests/integration/looker/test_looker_simple_metric.py
+++ tests/integration/looker/test_looker_simple_metric.py
@@ -1,5 +1,5 @@
 # tests/integration/test_simple_metric.py
-from tests.integration.helpers import generate_metricflow_results, generate_looker_results, generate_cube_results, do_query_results_match
+from tests.integration.helpers import generate_metricflow_results, generate_looker_results, do_query_results_match
 
 def test_simple_metric(bq_client, lkr_sdk):
     generate_metricflow_results(mf_command='mf query --metrics order_total', results_table='simple_metric', bq_client=bq_client, gcloud_project_id='fresh-iridium-428713-j5')
