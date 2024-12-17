c4baff7aac6dfc71823fb78d463aa34574d6526e
benw-at-birdie <55486471+benw-at-birdie@users.noreply.github.com>
2024-12-17 11:10:07 +0000

    build: fix requirements for pip install

--- setup.py
+++ setup.py
@@ -5,6 +5,12 @@ setup(
     version='0.1',
     py_modules=['mf_translate', 'mf_compare_query'],
     packages=find_packages(),
+    install_requires=[
+        'pandas>=1.5.0,<2.3.0',
+        'tabulate>=0.9.0,<1.0.0',
+        'looker-sdk>=23.0.0,<25.0.0',
+        'lkml>=1.3.0,<1.4.0',
+    ],
     entry_points={
         'console_scripts': [
             'mf-translate=mf_translate:main',
